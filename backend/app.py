a197cf77a5b4d76610323e4a727df84e40c615bb
gautham0914 <gauthamgongada.mcg@gmail.com>
2025-11-22 17:19:28 -0500

    Update: dotenv integration and gitignore cleanup

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,26 @@
 from flask import Flask, jsonify
 import mysql.connector
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+
 app = Flask(__name__)
 
 # Database Connection
 def get_connection():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Gautham@123", 
-        database="salesdb"
+        host="DB_HOST",
+        user="DB_USER",
+        password="DB_PASSWORD", 
+        database="DB_NAME"
     )
     return conn
 
--- etl/pipeline.py
+++ etl/pipeline.py
@@ -3,6 +3,18 @@ import pandas as pd
 import mysql.connector
 import logging
 import time
+#for python to read .env file for credentials
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+
 
 
 # Create a log file to track each pipeline run
@@ -25,10 +37,10 @@ def load_to_mysql(df):
     logging.info("Connecting to MySQL...")
 
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Gautham@123", 
-        database="salesdb"
+        host="DB_HOST",
+        user="DB_USER",
+        password="DB_PASSWORD", 
+        database="DB_NAME"
     )
     cur = conn.cursor()
 
@@ -84,3 +96,4 @@ def run_pipeline():
 
 if __name__ == "__main__":
     run_pipeline()
+
