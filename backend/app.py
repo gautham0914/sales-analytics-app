669313747f18ad2a02f0aaf212b11e8ec908ce4b
gautham0914 <gauthamgongada.mcg@gmail.com>
2025-11-25 18:12:47 -0500

    Enabled CORS

--- backend/app.py
+++ backend/app.py
@@ -194,7 +194,7 @@ def publisher_sales():
 
 import os
 
-if __name__ == "__main__":
+if __name__ =="__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
